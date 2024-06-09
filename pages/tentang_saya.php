f9f3d3798a1327fce31b94718888a3cc41c440ea
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-09 11:59:36 +0800

    Menambahkan Halaman Tentang Saya

---
+++ pages/tentang_saya.php
@@ -0,0 +1,33 @@
+<?php include('../partials/header.php'); ?>
+<?php include('../partials/sidebar.php'); ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="main-content">
+                <?php include('../partials/topbar.php') ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <h2 class="text-dark">Tentang Saya</h2>
+                    <hr>
+                    <div class="col-12 mb-4">
+                        <div class="card border-left-primary shadow h-100 py-2">
+                            <div class="card-body">
+                                <div class="row no-gutters align-items-center">
+                                    <div class="col mr-2">
+                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                            Terima Kasih Telah Berkunjung ke Web Hasil Praktikum Pemrograman Web 2
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+                <!-- End Page Content -->
+            
+            </div>
+            <!-- Main Content END -->
+            <?php include('../partials/footer.php'); ?>
