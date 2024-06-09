9f68475c2c89c59b5f9a52676784d906cf0f30e8
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-09 12:14:04 +0800

    Memperbarui Halaman Tentang Saya

--- pages/tentang_saya.php
+++ pages/tentang_saya.php
@@ -11,13 +11,45 @@
 
                     <h2 class="text-dark">Tentang Saya</h2>
                     <hr>
-                    <div class="col-12 mb-4">
-                        <div class="card border-left-primary shadow h-100 py-2">
-                            <div class="card-body">
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col mr-2">
-                                        <div class="h5 mb-0 font-weight-bold text-gray-800">
-                                            Terima Kasih Telah Berkunjung ke Web Hasil Praktikum Pemrograman Web 2
+                    <div class="row">
+                        <div class="col-9 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="h5 mb-0 text-gray-800">
+                                                <table>
+                                                    <tr>
+                                                        <th>Nama</th>
+                                                        <td class="p-3">: Abdurrahim</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Kelas</th>
+                                                        <td class="p-3">: 4D TI REG PAGI BJB</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Fakultas </th>
+                                                        <td class="p-3">: Teknologi Informasi</td>
+                                                    </tr>
+                                                    <tr>
+                                                        <th>Prodi</th>
+                                                        <td class="p-3">: Teknik Informatika</td>
+                                                    </tr>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-3 mb-4">
+                            <div class="card border-left-primary shadow h-100 py-2">
+                                <div class="card-body">
+                                    <div class="row no-gutters align-items-center">
+                                        <div class="col mr-2">
+                                            <div class="h5 mb-0 font-weight-bold text-gray-800">
+                                                <img src="../assets/img/logo-uniska.png" width="100%" alt="">
+                                            </div>
                                         </div>
                                     </div>
                                 </div>
