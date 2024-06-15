6160c5b1e1628f16d225fcca9f599a18288252b3
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-15 20:58:54 +0800

    Menambahkan form input di halaman edit karyawan

--- pages/karyawan_edit.php
+++ pages/karyawan_edit.php
@@ -34,7 +34,78 @@
                             <h6 class="m-0 font-weight-bold text-primary">Detail Karyawan</h6>
                         </div>
                         <div class="card-body">
-                            
+                            <form action="" method="post">
+                            <div class="mb-3 row">
+                                    <label for="nik" class="col-sm-2 col-form-label">NIK</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="nik" name="nik" placeholder="Masukkan NIK" maxlength="16" required>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="nama" name="nama" placeholder="Masukkan Nama" required>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="tempatLahir" class="col-sm-2 col-form-label">Tempat Lahir</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="tempatLahir" name="tempatLahir" placeholder="Masukkan Tempat Lahir" required>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="tanggalLahir" class="col-sm-2 col-form-label">Tanggal Lahir</label>
+                                    <div class="col-sm-10">
+                                        <input type="date" class="form-control" id="tanggalLahir" name="tanggalLahir" placeholder="Masukkan Tanggal Lahir" required>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                                    <div class="col-sm-10">
+                                        <textarea class="form-control" id="alamat" name="alamat" placeholder="Masukkan Alamat"></textarea>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="noTelepon" class="col-sm-2 col-form-label">No Telepon</label>
+                                    <div class="col-sm-10">
+                                        <input type="text" class="form-control" id="noTelepon" name="noTelepon" placeholder="Masukkan No Telepon" maxlength="13" required>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="jabatan" class="col-sm-2 col-form-label">Jabatan</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-select form-control" id="jabatan" aria-label="Default select example">
+                                            <option selected>Pilih Jabatan</option>
+                                            <option value="Operator">Operator</option>
+                                            <option value="Leader">Leader</option>
+                                            <option value="Supervisor">Supervisor</option>
+                                            <option value="Manage">Manager</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label for="status" class="col-sm-2 col-form-label">Status</label>
+                                    <div class="col-sm-10">
+                                        <select class="form-select form-control" id="status" aria-label="Default select example">
+                                            <option selected>Pilih Status</option>
+                                            <option value="Outsourcing">Outsourcing</option>
+                                            <option value="Kontrak">Kontrak</option>
+                                            <option value="Tetap">Tetap</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="mb-3 row">
+                                    <label class="col-sm-2 col-form-label">&nbsp;</label>
+                                    <div class="col-sm-10">
+                                        <button type="submit" class="btn btn-primary" name="tambahKaryawan">
+                                            Tambah Karyawan
+                                        </button>
+                                        <button type="reset" class="btn btn-warning">
+                                            Reset
+                                        </button>
+                                    </div>
+                                </div>
+                            </form>
                         </div>
                     </div>
                     <!-- End Form card -->
