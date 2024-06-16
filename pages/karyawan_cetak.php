ca091d3198e9f71541c4d7ebd692f8f56df8f585
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-16 10:22:40 +0800

    Menambahkan card header di halaman karyawan_cetak.php

--- pages/karyawan_cetak.php
+++ pages/karyawan_cetak.php
@@ -6,6 +6,25 @@
             <div id="main-content">
                 <?php include('../partials/topbar.php') ?>
 
+                <div class="col-12 mb-4 px-0">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-12 col-sm-6">
+                                    <h4 class="page-header-title mb-0 text-dark">
+                                        <i class="fa-regular fa-user mr-2"></i> Laporan Data Karyawan
+                                    </h4>
+                                </div>
+                                <div class="col-12 col-sm-6 mt-3 mt-sm-0 d-flex justify-content-end">
+                                    <a class="btn btn-light text-primary" href="./karyawan_add.php" onclick="window.print();">
+                                        <i class="fa-solid fa-print"></i> Cetak Data Karyawan
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
