c8fa1bbda57248cc2cd8f78b7d297d097ee397fb
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-16 16:51:36 +0800

    Menambahkan struktur awal metode tambahKaryawan di KaryawanController

--- controllers/karyawan_controller.php
+++ controllers/karyawan_controller.php
@@ -3,4 +3,15 @@
 include '../includes/connection.php';
 include '../includes/library.php';
 
+class KaryawanController {
+    // Tambah Karyawan
+    public function tambahKaryawan($nik, $nama, $tempatLahir, $tanggalLahir, $alamat, $noTelepon, $jabatan, $status) {
+        
+    }
+    // END Tambah Karyawan 
+
+}
+
+
+
 ?>
