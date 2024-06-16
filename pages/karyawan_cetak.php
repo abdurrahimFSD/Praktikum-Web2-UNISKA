013c84fb1b699fb2ebaeb6c629b343c1206cc7f2
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-16 10:34:22 +0800

    Memperbarui halaman karyawan cetak

--- pages/karyawan_cetak.php
+++ pages/karyawan_cetak.php
@@ -16,7 +16,7 @@
                                     </h4>
                                 </div>
                                 <div class="col-12 col-sm-6 mt-3 mt-sm-0 d-flex justify-content-end">
-                                    <a class="btn btn-light text-primary" href="./karyawan_add.php" onclick="window.print();">
+                                    <a class="btn btn-light text-primary" onclick="window.print();">
                                         <i class="fa-solid fa-print"></i> Cetak Data Karyawan
                                     </a>
                                 </div>
@@ -28,6 +28,41 @@
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
+                    <div class="card shadow mb-4">
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover" id="dataTable" width="100%" cellspacing="0">
+                                    <thead>
+                                        <tr>
+                                            <th>NO</th>
+                                            <th>NIK</th>
+                                            <th>Nama</th>
+                                            <th>Tempat Lahir</th>
+                                            <th>Tanggal Lahir</th>
+                                            <th>Alamat</th>
+                                            <th>No Telepon</th>
+                                            <th>Jabatan</th>
+                                            <th>Status</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        <tr>
+                                            <td>1</td>
+                                            <td>11001</td>
+                                            <td>Haaland</td>
+                                            <td>Norway</td>
+                                            <td>2011/04/25</td>
+                                            <td>Norway City</td>
+                                            <td>081347200001</td>
+                                            <td>Manajer</td>
+                                            <td>Tetap</td>
+                                        </tr>
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
+                    </div>
+                
                 </div>
                 <!-- End Page Content -->
             
