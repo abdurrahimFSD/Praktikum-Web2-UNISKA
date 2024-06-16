2e051e913a4c606a1aa2848d5742b5b0681a0a08
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-16 11:08:06 +0800

    Menambahkan koneksi ke database

--- includes/connection.php
+++ includes/connection.php
@@ -7,10 +7,8 @@ $databaseName = "uniska_latihan_app";
 
 $connection = mysqli_connect($serverName, $username, $password, $databaseName);
 
-if($connection) {
-    // echo "Koneksi berhasil";
-} else {
-    die("Koneksi gagal : " . mysqli_connect_error());
+if (mysqli_connect_errno()) {
+    echo "Gagal melakukan koneksi ke database : " . mysqli_connect_error();
 }
 
 ?>
