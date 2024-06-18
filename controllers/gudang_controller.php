f2e757d0bdb4cb2e1818fd66cc1825c9e9fa2292
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-18 10:54:39 +0800

    Menambahkan function tambahGudang ke dalam GudangController untuk menangani penambahan data gudang dari form

--- controllers/gudang_controller.php
+++ controllers/gudang_controller.php
@@ -5,9 +5,25 @@ include '../includes/library.php';
 session_start();
 
 class GudangController {
-
+    // Tambah Gudang
+    public function tambahGudang($idGudang, $namaGudang, $lokasiGudang, $luasGudang) {
+        
+    }
+    // END Tambah Gudang
 }
 
 $gudangController = new GudangController();
 
+
+// Tambah Gudang
+if(isset($_POST['tambahGudang'])) {
+    $idGudang = $_POST['idGudang'];
+    $namaGudang = $_POST['namaGudang'];
+    $lokasiGudang = $_POST['lokasiGudang'];
+    $luasGudang = $_POST['luasGudang'];
+
+    // Memanggil function tambahGudang dengan data dari form
+    $gudangController->tambahGudang($idGudang, $namaGudang, $lokasiGudang, $luasGudang);
+}
+// END Tambah Gudang
 ?>
