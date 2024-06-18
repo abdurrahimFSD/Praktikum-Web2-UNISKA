e4db4d2ba52f30225e23d642e6e1caa78eb7c511
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-18 09:48:08 +0800

    Menambahkan struktur awal dan inklusi parsial ke gudang_add.php

---
+++ pages/gudang_add.php
@@ -0,0 +1,19 @@
+<?php include('../partials/header.php'); ?>
+<?php include('../partials/sidebar.php'); ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="main-content">
+                <?php include('../partials/topbar.php') ?>
+
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+
+
+                </div>
+                <!-- End Page Content -->
+
+            </div>
+            <!-- Main Content END -->
+            <?php include('../partials/footer.php'); ?>
