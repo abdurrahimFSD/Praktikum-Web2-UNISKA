bf925d233816bbdde642e8f995f20390dff9ee35
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-18 11:39:36 +0800

    Menambahkan logika untuk mengecek apakah nama gudang sudah ada sebelumnya sebelum menambahkannya ke database, serta menambahkan fungsi untuk menambahkan data gudang ke database

--- controllers/gudang_controller.php
+++ controllers/gudang_controller.php
@@ -7,7 +7,32 @@ session_start();
 class GudangController {
     // Tambah Gudang
     public function tambahGudang($idGudang, $namaGudang, $lokasiGudang, $luasGudang) {
-        
+        // Mengakses koneksi database yang telah dibuat di file connection.php
+        global $connection;
+
+        // Cek apakah Nama Gudang sudah ada
+        $queryCekNamaGudang = "SELECT * FROM gudang WHERE nama_gudang = '$namaGudang'";
+        $resultCekNamaGudang = mysqli_query($connection, $queryCekNamaGudang);
+
+        if(mysqli_num_rows($resultCekNamaGudang) > 0) {
+            // Jika Nama Gudang sudah ada maka redirect ke halaman gudang_add.php 
+            $_SESSION['namaGudangSudahAda'] = $namaGudang;
+            header("location: ../pages/gudang_add.php");
+            exit;
+        }
+
+        // Query untuk menambahkan dataGudang ke database
+        $queryAddGudang = "INSERT INTO gudang (nama_gudang, lokasi_gudang, luas_gudang) VALUES ('$namaGudang', '$lokasiGudang', '$luasGudang')";
+        // Eksekusi query
+        $resultAddGudang = mysqli_query($connection, $queryAddGudang);
+
+        // Cek apakah query berhasil di jalankan
+        if($resultAddGudang) {
+            // Jika berhasil, redirect ke halaman gudang_add.php
+            $_SESSION['doneTambahGudang'] = true;
+            header("location: ../pages/gudang_add.php");
+            exit;
+        }
     }
     // END Tambah Gudang
 }
