1c1d6918ab2eebc900323dcff68dafc84118dec1
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-18 09:34:42 +0800

    Menambahkan card header di halaman gudang data

--- pages/gudang_data.php
+++ pages/gudang_data.php
@@ -6,6 +6,25 @@
             <div id="main-content">
                 <?php include('../partials/topbar.php') ?>
 
+                <div class="col-12 mb-4 px-0">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-12 col-sm-6">
+                                    <h4 class="page-header-title mb-0 text-dark">
+                                        <i class="fa-solid fa-warehouse mr-2"></i> Data Gudang
+                                    </h4>
+                                </div>
+                                <div class="col-12 col-sm-6 mt-3 mt-sm-0 d-flex justify-content-end">
+                                    <a class="btn btn-light text-primary" href="">
+                                        <i class="fa-solid fa-square-plus"></i> Tambah Gudang Baru
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
