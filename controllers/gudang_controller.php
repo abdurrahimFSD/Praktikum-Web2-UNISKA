4cb6992befac6cfec7d7f0bc08ebf2fa1f0a9931
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-18 10:48:56 +0800

    Menyiapkan struktur dasar untuk pengontrol gudang dalam gudang_controller.php

---
+++ controllers/gudang_controller.php
@@ -0,0 +1,13 @@
+<?php
+
+include '../includes/connection.php';
+include '../includes/library.php';
+session_start();
+
+class GudangController {
+
+}
+
+$gudangController = new GudangController();
+
+?>
