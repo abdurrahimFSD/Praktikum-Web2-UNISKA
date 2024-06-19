ab438833c6adc5681ef104d72a449ade1a160ff6
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-19 17:47:46 +0800

    Menambahkan card header di halaman distribusi_karyawan.php

--- pages/distribusi_karyawan.php
+++ pages/distribusi_karyawan.php
@@ -6,6 +6,20 @@
             <div id="main-content">
                 <?php include('../partials/topbar.php') ?>
 
+                <div class="col-12 mb-4 px-0">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-12">
+                                    <h4 class="page-header-title mb-0 text-dark">
+                                        <i class="fas fa-users-cog mr-2"></i> Pilih Karyawan Yang Akan Didistribusikan
+                                    </h4>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
 
