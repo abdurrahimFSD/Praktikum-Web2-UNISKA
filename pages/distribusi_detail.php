d86e71fad938b6b92bd5473ffcd629f2b804c19d
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-22 11:40:46 +0800

    Menambahkan struktur awal dan inklusi parsial ke distribusi_add.php

---
+++ pages/distribusi_detail.php
@@ -0,0 +1,21 @@
+<?php include('../partials/header.php'); ?>
+<?php include('../partials/sidebar.php'); ?>
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+            <!-- Main Content -->
+            <div id="main-content">
+                <?php include('../partials/topbar.php') ?>
+
+
+                <?php include('../controllers/distribusi_controller.php'); ?>
+                
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+
+                </div>
+                <!-- End Page Content -->
+
+            </div>
+            <!-- Main Content END -->
+            <?php include('../partials/footer.php'); ?>
