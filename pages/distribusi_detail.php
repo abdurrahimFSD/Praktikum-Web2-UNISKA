ff47dc7750ea400bd54db03f3f1e28bbeeb59e7c
Abdurrahim <abdurrahim22code@gmail.com>
2024-06-22 11:47:41 +0800

    Menambahkan card header di halaman distribusi_detail.php

--- pages/distribusi_detail.php
+++ pages/distribusi_detail.php
@@ -6,8 +6,24 @@
             <div id="main-content">
                 <?php include('../partials/topbar.php') ?>
 
-
-                <?php include('../controllers/distribusi_controller.php'); ?>
+                <div class="col-12 mb-4 px-0">
+                    <div class="card h-100">
+                        <div class="card-body">
+                            <div class="row align-items-center">
+                                <div class="col-12 col-sm-6">
+                                    <h4 class="page-header-title mb-0 text-dark">
+                                        <i class="fa-solid fa-warehouse"></i> Detail Distribusi
+                                    </h4>
+                                </div>
+                                <div class="col-12 col-sm-6 mt-3 mt-sm-0 d-flex justify-content-end">
+                                    <a class="btn btn-light text-primary" href="./distribusi_karyawan.php">
+                                        <i class="fa-solid fa-arrow-left"></i> Kembali Ke Distribusi Data
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
                 
                 <!-- Begin Page Content -->
                 <div class="container-fluid">
